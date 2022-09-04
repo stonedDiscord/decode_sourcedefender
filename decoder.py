d95955722da113a08f67831b0bc1c8133d42e3d4
stonedDiscord <Tukz@gmx.de>
2022-09-04 17:54:10 +0200

    initial commit

---
+++ decoder.py
@@ -0,0 +1,32 @@
+import sys
+from base64 import a85decode
+from zlib import decompress
+from tgcrypto import ctr256_decrypt
+
+
+infile = sys.argv[1]
+f = open(infile, 'r', encoding='utf-8')
+encrypted = f.read()
+f.close()
+stripped = encrypted.replace("-----BEGIN SOURCEDEFENDER FILE-----","")
+stripped = stripped.replace("------END SOURCEDEFENDER FILE------","")
+stripped = stripped.replace(" ","")
+stripped = stripped.replace("\n","")
+stripped = stripped.replace("\n","")
+
+decoded = a85decode(stripped)
+print(decoded)
+
+key = bytes(32)
+iv = bytes(16)
+state = bytes(1)
+
+#decoded= ctr256_decrypt(decoded, key, iv, state)
+#print(decrypted)
+
+nf = open(infile + ".bin", "wb")
+nf.write(decoded)
+nf.close()
+
+#decompressed = decompress(decoded)
+#print(decompressed)
